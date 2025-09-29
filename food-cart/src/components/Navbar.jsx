ec03a7c1102efbe248a61f4b0c71ae7281c291a1
Vasuki t <iamvasukithangaraj@gmail.com>
2025-09-29 18:08:16 +0530

    added responsiveness for navbar

--- food-cart/src/components/Navbar.jsx
+++ food-cart/src/components/Navbar.jsx
@@ -7,21 +7,44 @@ function Navbar() {
   const toggleMenu = () => setIsOpen(!isOpen);
   return (
     <div className="bg-green-300 shadow-lg fixed w-full">
+      {/* responsiveness */}
       <div className="max-w-7xl mx-auto px-6 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16 items-center"> 
+        <div className="flex justify-between h-16 items-center">
           <div className="text-2xl font-extrabold text-red-500">FoodCart</div>
 
-          <div className=" hidden md:flex">
-            <a href="#">Home</a>
-            <a href="#">Foods</a>
-            <a href="#">offers</a>
-            <a href="#">Help</a>
+          {/* Laptop & desktop */}
+          <div className=" hidden md:flex space-x-6">
+             <a  href="#">Home</a>
+             <a  href="#">Foods</a>
+             <a  href="#">offers</a>
+             <a  href="#">Help</a>
+          </div>
+
+          {/* Mobile */}
+          <div className="md:hidden">
+            <button onClick={toggleMenu}>
+              {isOpen ? <X /> : <TextAlignJustify />}
+            </button>
           </div>
         </div>
-        {/* https://dummyjson.com/recipes */}
       </div>
+      {/* mobile menu design */}
+      {
+        isOpen && (
+        <div className="md:hidden bg-green-300 shadow-lg p-4 space-y-5 ">
+            <a className="block"  href="#">Home</a>
+             <a  className="block"   href="#">Foods</a>
+             <a   className="block"  href="#">offers</a>
+             <a   className="block"  href="#">Help</a>
+
+        </div>
+        )
+      }
     </div>
   );
 }
 
 export default Navbar;
+{
+  /* https://dummyjson.com/recipes */
+}
