ac23af13adb8a5b55b2e59ae92847828f36255a8
Vasuki t <iamvasukithangaraj@gmail.com>
2025-09-30 15:21:42 +0530

    added some styles

--- food-cart/src/components/Recipes.jsx
+++ food-cart/src/components/Recipes.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {Star} from 'lucide-react';
+import { Star } from "lucide-react";
 
 function Recipes() {
   const [recipes, setRecipes] = useState([]);
@@ -21,16 +21,17 @@ function Recipes() {
   }, []);
 
   return (
-    <div className="font-mono ... p-4 md:p-8 bg-gray-600 min-h-screen">
-      <h3 className="text-2xl md:text-3xl font-bold text-center text-white p-4 mb-6">
+    <div className="font-mono ... p-4 md:p-8 bg-gray-200 min-h-screen  ">
+
+      <h3 className="text-2xl md:text-3xl font-bold text-center text-black p-4 mb-6">
         Our Recipes
       </h3>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7 ">
         {recipes.map((item) => (
           <div
             key={item.id}
-            className="bg-gray-100 shadow-md rounded p-5 flex flex-col items-center"
+            className="bg-gray-100 shadow-md rounded p-5 flex flex-col items-center " 
           >
             <img
               className="h-40 w-auto mb-4 object-contain rounded-3xl"
@@ -41,15 +42,14 @@ function Recipes() {
               {item.name}
             </h4>
 
-            <div className="font-semibold text-lg mb-2 flex items-center gap-15">
-              <p className="font-bold text-gray-600">{item.cuisine}</p>
-              <p className="text-red-600 flex flex-row"><Star size={23} />{item.rating}</p>
+            <div className="font-semibold text-lg mb-2 flex justify-between gap-15 ">
+              {/* Display Random Price */}
+              <p className="text-green-600 font-bold text-xl mb-2">
+                ${item.price}
+              </p>
+              <p className="bg-red-500 p-1 px-1 flex flex-row text-white rounded "><Star size={23} />
+                  {item.rating}</p>
             </div>
-
-            {/* Display Random Price */}
-            <p className="text-green-600 font-bold text-xl mb-2">
-              ${item.price}
-            </p>
 
             <button className="bg-lime-400 px-6 py-2 cursor-pointer text-red-700 rounded text-lg">
               View Recipe
